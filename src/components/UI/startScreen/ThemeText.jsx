8a133f65a19126b82dce7a78d38de7100e146f9d
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-06-02 00:11:04 +0300

    animate start screen

--- src/components/UI/startScreen/ThemeText.jsx
+++ src/components/UI/startScreen/ThemeText.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import cl from './Properties.module.css'
+import {motion} from "framer-motion";
 
 const ThemeText = () => {
-    return (
-            <div className={cl.themeTextWrapper}>
-                <div className={cl.firstLine}>
-                    UX/UI &
-                </div>
-                <div className={cl.secondLine}>
-                    MOBILE APP
-                </div>
-                <div className={cl.thirdLine}>
-                    <div className={cl.smallText}>
-                        <div className={cl.smallTextInnerWrapper}>
-                            just a design and strategy with an adaptive approach to problem solving
-                        </div>
-                    </div>
 
-                    <div className={cl.bigText}>
-                        DESIGNER
+    const textAnimation = {
+        hidden: {
+            y: 75,
+            opacity: 0,
+        },
+        visible: custom => ({
+            y: 0,
+            opacity: 1,
+            transition: {delay: custom * 0.1, duration: 0.5},
+        })
+    }
+
+
+    return (
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            className={cl.themeTextWrapper}
+        >
+            <motion.div
+                custom={1} variants={textAnimation}
+                className={cl.firstLine}
+            >
+                UX/UI &
+            </motion.div>
+            <motion.div
+                custom={2} variants={textAnimation}
+                className={cl.secondLine}
+            >
+                MOBILE APP
+            </motion.div>
+            <div className={cl.thirdLine}>
+                <motion.div
+                    custom={3} variants={textAnimation}
+                    className={cl.smallText
+                    }>
+                    <div className={cl.smallTextInnerWrapper}>
+                        just a design and strategy with an adaptive approach to problem solving
                     </div>
-                </div>
+                </motion.div>
+
+                <motion.div
+                    custom={4} variants={textAnimation}
+                    className={cl.bigText}
+                >
+                    DESIGNER
+                </motion.div>
             </div>
+        </motion.div>
     );
 };
 
