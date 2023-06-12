5e986f8c38270ec0723e49d5fddf0fb8ebce1df9
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-06-12 12:15:46 +0300

    change font-weight in accordion and change crossButton

--- src/components/UI/buttons/crossButton/crossButton.jsx
+++ src/components/UI/buttons/crossButton/crossButton.jsx
@@ -6,16 +6,18 @@ const CrossButton = ({descrText}) => {
     return (
         descrText
             ?
-            <svg className={cl.buttonOn} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={cl.buttonOff} width="32" height="32" viewBox="0 0 32 32" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
                 <circle cx="16" cy="16" r="16" fill="black"/>
-                <path d="M22.2454 11.6791L10.7547 21.3209" stroke="white" strokeLinecap="round"/>
-                <path d="M10.7546 11.6791L22.2453 21.3209" stroke="white" strokeLinecap="round"/>
+                <path d="M20.8728 11.6483L12.3197 20.0652" stroke="white" strokeLinecap="round"/>
+                <path d="M12 12L20.8715 20.0806" stroke="white" strokeLinecap="round"/>
             </svg>
             :
-            <svg className={cl.buttonOff} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className={cl.buttonOn} width="32" height="32" viewBox="0 0 32 32" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
                 <circle cx="16" cy="16" r="16" fill="#F3F3F3"/>
-                <path d="M16.5 9V24" stroke="black" strokeLinecap="round"/>
-                <path d="M9 16.5H24" stroke="black" strokeLinecap="round"/>
+                <path d="M16 11V23" stroke="black" strokeLinecap="1.25" strokeLinecap="round"/>
+                <path d="M10 17H22" stroke="black" strokeLinecap="1.25" strokeLinecap="round"/>
             </svg>
 
 
